c232cbf01be104000933dd07bc6316af25d9f2fd
KOWSHIK BORRA <kowshik8305@gmail.com>
2026-07-23 07:58:31 +0530

    Add files via upload

---
+++ DAY 2/variable operation.py
@@ -0,0 +1,26 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=12
+>>> a
+12
+>>> A=13
+>>> A
+13
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c=12,13,14
+>>> a
+12
+>>> c
+14
+>>> b
+13
+>>> 
+>>> 
+>>> 
+>>> 
---
+++ DAY 3/tpyecasting.py
@@ -0,0 +1,8 @@
+int - float,str,complex,bool
+float -int,bool,complex,str
+str - list,set,tuple, if str contains integers it will cast int, float,complex
+complex - str,bool
+list - tuple,set,bool,str,
+tuple - list, str,bool,set
+bool - int,str,list,tuple,set,float,complex,dict
+
